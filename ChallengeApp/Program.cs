c78ded43ad6a3860716b3adbd7f02e28569562c3
MENETHORI <menethor@wp.pl>
2024-07-04 20:26:11 +0200

    zadanie dzien 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -3,19 +3,15 @@ string  sex = "kobieta";
 // char = "m";
 int age = 28;
 
-if (name == "kobieta" && age < 30)
+if (sex == "kobieta" && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (name == "Ewa" && age < 30)
+else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
 else if (sex == "mezczyzna" && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    return;
 }
