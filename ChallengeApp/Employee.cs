f1fb95dfec44ed8fdc46c051f3a1c95c52d97c04
MENETHORI <menethor@wp.pl>
2024-07-10 19:20:28 +0200

    zadanie dzien 6

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Employee(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void AddScore(int points)
+        { 
+            this.score.Add(points);
+        }
+        public int GetScore()
+        {
+            return this.score.Sum();
+        }
+
+
+    }
+}
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,17 +1,37 @@
-﻿string name = "Ewa";
-string  sex = "kobieta";
-// char = "m";
-int age = 28;
+﻿using ChallengeApp;
 
-if (sex == "kobieta" && age < 30)
+Employee first = new Employee("Nowak", 23);
+Employee second = new Employee("Kowalski", 24);
+Employee third = new Employee("Sienkiewicz", 25);
+
+first.AddScore(1);
+second.AddScore(2);
+third.AddScore(3);
+
+first.AddScore(1);
+second.AddScore(1);
+third.AddScore(3);
+
+first.AddScore(1);
+second.AddScore(1);
+third.AddScore(3);
+
+int resultFirst = first.GetScore();
+int resultSecond = second.GetScore();
+int resultThird = third.GetScore();
+
+if (resultFirst > resultSecond && resultFirst > resultThird)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    Console.WriteLine("Zwyciesca to " + first.Name + " Punktów " + resultFirst + " wiek " + first.Age);
 }
-else if (name == "Ewa" && age == 30)
+else
 {
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (sex == "mezczyzna" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    if (resultSecond > resultThird && resultSecond > resultFirst)
+    {
+        Console.WriteLine("Zwyciesca to " + second.Name + " Punktów " + resultSecond + " wiek " + second.Age);
+    }
+    else
+    {
+        Console.WriteLine("Zwyciesca to " + third.Name + " Punktów " + resultThird + " wiek " + third.Age);
+    }
 }
